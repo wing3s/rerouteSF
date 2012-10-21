bac82ba8c5065f5c880390d3acc8ddf0c06f8fb7
wing3s <wing3s@gmail.com>
2012-10-21 03:13:19 -0700

    add bootstrap and dropdown for mainpage

--- application/views/mainpage.php
+++ application/views/mainpage.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lan="en">
 <head>
 	<script type="text/javascript" src="https://ajax.microsoft.com/ajax/jquery/jquery-1.8.0.min.js"></script>
@@ -11,15 +12,37 @@
 </head>
 
 <body>
-<div class='visible-phone'>
-
-
+<div class='row'>
+	<ul class='nav nav-pills'>
+		<li class='dropdown'>
+			<a class='dropdown-toggle' id='startDrop' role='button' data-toggle='dropdown' href="#">Origin<b class='caret'></b>
+			</a>
+			<ul class='dropdown-menu' role='menu' aria-labelledby='startDrop'>
+				<?php
+					foreach($allStopName as $stopName)
+					{
+						echo "<li><a tabindex='-1' href='#'>$stopName</a></li>";
+					}
+				?>
+			</ul>
+		</li>
+	</ul>
+</div>
+<div class='row'>
+	<ul class='nav nav-pills'>
+		<li class='dropdown'>
+			<a class='dropdown-toggle' id='endDrop' role='button' data-toggle='dropdown' href="#">Destination<b class='caret'></b>
+			</a>
+			<ul class='dropdown-menu' role='menu' aria-labelledby='endDrop'>
+				<?php
+					foreach($allStopName as $stopName)
+					{
+						echo "<li><a tabindex='-1' href='#'>$stopName</a></li>";
+					}
+				?>
+			</ul>
+		</li>
+	</ul>
 </div>
 </body>
 </html>
-<?php
-	foreach($allStopName as $stopName)
-	{
-		echo $stopName;
-	}
-?>
