a474659dfb584f7da42f78ed4fa514e6b557482c
Lans <lansoulot@gmail.com>
2012-10-21 13:57:42 -0700

    getTimeTakeBetweenStops

--- application/models/backend.lans.php
+++ application/models/backend.lans.php
@@ -44,14 +44,48 @@ class backend extends CI_Model
 		return 1;
 	}
 
-	public function getStopsBetween($stop1,$stop2)
+	public function getTimeTakeBetweenStops($stop1, $stop2, $routeNum)
 	{
-		$stopArr	= array(
-			'777',
-			'888',
-			'999',
-			);
-		return;
+		$startTimestamp;
+		$endTimestamp;		
+
+
+		$sql = "SELECT *
+			FROM dw_sfmta_bus_load_matrix
+			WHERE route_num = $routeNum 
+			AND day_of_week = 1";
+		
+		$result = $this->dbBI->query($sql)->result_array();
+		$count = 0;
+		$indexToStart = 0;
+		$previousRecordSeqStopSeq = 0;
+		foreach($result as $row) {
+			$count++;
+			if($row->seq_stop_id < $previousRecordSeqStopID) {
+				$indexToStart = $count;
+			}
+			$previousRecordSeqStopID = $row->seq_stop_id;
+		}
+		
+		$result2 = $this->dbBI->query($sql)->result_array();
+		$count = 0;
+		foreach($result2 as $row) {
+			$count++;
+			if($count < $indexToStart) {
+				continue;
+			}
+			if($row->stop_name == $stop1) {
+				$startTimestamp = $row->timestamp;
+			}
+			if($row->stop_name == $stop2) {
+				$endTimestamp = $row->timestamp;
+				break;
+			}
+		}
+
+		$timetakes = ( $endTimestamp - $startTimestamp ) / 1000;
+
+		return $timetakes;
 	}
 }
 ?>
